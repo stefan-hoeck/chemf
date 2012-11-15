59ea1a7c5616c9aa513c2389cbbda2aa22495d31
efasckenoth <efasckenoth@gmail.com>
2012-11-15 07:27:32 +0100

    testing parallel arrays

--- src/main/scala/chemf/Main.scala
+++ src/main/scala/chemf/Main.scala
@@ -6,10 +6,10 @@ import scalaz._, Scalaz._
 
 object Main extends testing.Benchmark {
   def run {
-    FJP.setParallelism(12)
+    FJP.setParallelism(9)
     def str = getClass.getResourceAsStream("zinc.txt")
     def source = scala.io.Source fromInputStream str getLines
-    val lines = source.toIndexedSeq
+    val lines = source.toArray
     def countImpHs(s: String) = smiles(s) fold (_ ⇒ 0, _ foldMap (_ hydrogens))
     def res = lines.par map countImpHs sum
 
